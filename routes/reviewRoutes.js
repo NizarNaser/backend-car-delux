b6734187d3d05e344b7d923ba2c03c316ce909be
NizarNaser <nizarnaser17@gmail.com>
2025-05-13 22:52:04 +0200

    commit10

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 const router = express.Router();
-import { createReview, getCarReviews } from '../controllers/reviewController';
+import { createReview, getCarReviews } from '../controllers/reviewController.js';
 
 router.post('/', createReview);
 router.get('/:carId', getCarReviews);
